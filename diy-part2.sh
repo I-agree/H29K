cbb917d6678a7cc98d73543ac26737f015c73964
I-agree <94974975+I-agree@users.noreply.github.com>
2026-04-18 15:06:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,19 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#!/bin/bash
+
+# 1. 创建目标目录（如果不存在）
+mkdir -p target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+
+# 2. 下载你的 dts 文件到指定位置
+# 注意：这里使用 raw 链接直接下载
+curl -fsSL https://raw.githubusercontent.com/aaaol/OpenWrt/master/Files/LEDE/HinLink_H29K/target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3528-opc-h29k.dts \
+    -o target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3528-opc-h29k.dts
+
+# 3. 修正 Makefile 以便内核识别新设备 (非常重要)
+# 这一步将设备型号加入到 rockchip 平台的内核编译列表中
+sed -i '/rk3528/a \ \ \ \ \ \ \ \ rk3528-opc-h29k.dtb \\' target/linux/rockchip/image/arm64.mk
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
