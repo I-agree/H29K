28c25e7fe05adaf3f6720144b8c3c3853b0064bf
I-agree <94974975+I-agree@users.noreply.github.com>
2026-04-19 16:23:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+#echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
 #echo 'src-git quectel_cm_5G https://github.com/I-agree/quectel_cm_5G.git' >> feeds.conf.default
