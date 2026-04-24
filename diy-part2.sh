c08fbd93d6b737b302ce049ac2b709747e240825
I-agree <94974975+I-agree@users.noreply.github.com>
2026-04-24 22:18:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -63,7 +63,7 @@ define Device/hinlink_h29k
   BOARD_ROOTFS_PARTSIZE := 1024
 
   IMAGES := sysupgrade.img
-  IMAGE/sysupgrade.img := boot-img | sdcard-img | pad-rootfs | pad-to 128k
+  IMAGE/sysupgrade.img := boot-common | boot-script | sdcard-img | append-metadata
   DEVICE_PACKAGES := kmod-usb3 uboot-rockchip-v8 kmod-usb-net-rtl8152 kmod-r8169 kmod-aic8800-sdio wpad-openssl dnsmasq-full kmod-mtk_t7xx kmod-usb-net-cdc-mbim uqmi kmod-usb-net-rndis-host kmod-usb-serial-option kmod-h29k-fb-st7789v luci-app-qmodem-next luci-i18n-qmodem-next-zh-cn luci-theme-argon fbv imagemagick wqy-microhei curl irqbalance luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn
 endef
 TARGET_DEVICES += hinlink_h29k
