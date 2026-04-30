7c0898072dbd73b7c701c0be32801a498ceab938
I-agree <94974975+I-agree@users.noreply.github.com>
2026-04-30 22:44:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # ======================== 【第2部分：资源准备】 ========================
 echo "✅ 正在执行基础资源下载..."
+# 创建必需目录
+mkdir -p target/linux/rockchip/dts/
+# DTS 设备树文件
+cp -f $GITHUB_WORKSPACE/rk3528-hinlink-h29k.dts target/linux/rockchip/dts/
 
 # 创建开机 LOGO 存放目录
 mkdir -p files/etc/config/screen bin/targets/rockchip/armv8
