22039c30998b1732c9d3f6dd523aa67bfdcfe702
I-agree <94974975+I-agree@users.noreply.github.com>
2026-05-07 14:07:30 +0800

    Add files via upload

---
+++ files/target/linux/rockchip/image/armv8.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2020 Sarah Maedel
+
+define Device/rk3528
+  SOC := rk3528
+  KERNEL_LOADADDR := 0x03000000
+endef
+
+define Device/hinlink_h29k
+  SOC := rk3528
+  SUBTARGET := armv8
+  DEVICE_VENDOR := HINLINK
+  DEVICE_MODEL := H29K
+  DEVICE_DTS := rk3528-hinlink-h29k
+  TRUSTED_FIRMWARE_A := rk3528
+  UBOOT_CONFIG := hinlink_h29k
+  KERNEL_LOADADDR := 0x00280000
+  KERNEL_ENTRYADDR := 0x00280000
+  DEVICE_UBOOT_IMAGE := u-boot-rockchip-hinlink_h29k.bin
+  DEVICE_COMPAT_VERSION := 1
+  SUPPORTED_DEVICES := hinlink_h29k
+  IMAGE/boot.bin := boot-scr | boot-kernel | boot-dtb
+  IMAGE/sysupgrade.img.gz := boot.bin | append-rootfs | pad-rootfs | check-size | gzip
+  DEVICE_PACKAGES := \
+    kmod-usb3 kmod-aic8800-sdio dnsmasq-full \
+    kmod-usb-net-cdc-mbim uqmi qmi-utils kmod-usb-serial-option kmod-usb-net-rndis-host \
+    luci-app-qmodem-next luci-i18n-qmodem-next-zh-cn \
+    luci-theme-argon imagemagick imagemagick-jpeg imagemagick-png imagemagick-gif curl \
+    luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn \
+    luci-app-bbr luci-i18n-bbr-zh-cn luci-mod-admin-full \
+    luci-app-irqbalance luci-i18n-irqbalance-zh-cn \
+    dnscrypt-proxy luci-app-dnscrypt-proxy luci-i18n-dnscrypt-proxy-zh-cn \
+    irqbalance luci-app-irqbalance luci-i18n-irqbalance-zh-cn -urngd
+endef
+TARGET_DEVICES += hinlink_h29k
