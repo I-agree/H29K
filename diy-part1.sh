29a163bda3faaf4d7a0a0df79c9ce9d43cf83fe5
I-agree <94974975+I-agree@users.noreply.github.com>
2026-05-10 12:09:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,8 @@
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
 # 无线网卡驱动
 echo 'src-git aic8800 https://github.com/radxa-pkg/aic8800.git;main' >> feeds.conf.default
-# 添加 Argon 主题源
-echo "src-git argon https://github.com/jerrykuku/luci-theme-argon.git" >> feeds.conf.default
+# 正确安装 argon 主题
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # ====================== 方案：全套切换为LEDE rk3528.dtsi + rk3528-pinctrl.dtsi ======================
 # 1. 清理OpenWrt原生冲突DTS和补丁
