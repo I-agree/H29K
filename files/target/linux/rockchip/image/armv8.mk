4580e7642b3617e62564b334a1a500ee986c7360
I-agree <94974975+I-agree@users.noreply.github.com>
2026-05-30 02:12:07 +0800

    Update armv8.mk

--- files/target/linux/rockchip/image/armv8.mk
+++ files/target/linux/rockchip/image/armv8.mk
@@ -19,13 +19,8 @@ define Device/hinlink_h29k
   BOOT_SCRIPT := mmc
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := \
-    kmod-usb3 kmod-aic8800-sdio dnsmasq-full \
+    kmod-usb3 kmod-aic8800-sdio -dnsmasq dnsmasq-full \
     kmod-usb-net-cdc-mbim uqmi qmi-utils kmod-usb-serial-option kmod-usb-net-rndis-host \
-    luci-app-qmodem-next luci-i18n-qmodem-next-zh-cn \
-    luci-theme-argon graphicsmagick curl \
-    luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn \
-    luci-app-bbr luci-i18n-bbr-zh-cn luci-mod-admin-full \
-    dnscrypt-proxy luci-app-dnscrypt-proxy luci-i18n-dnscrypt-proxy-zh-cn \
-    irqbalance luci-app-irqbalance luci-i18n-irqbalance-zh-cn u-boot-hinlink-h29k-rk3528
+    kmod-usb-net-cdc-ether kmod-usb-wdm u-boot-hinlink-h29k-rk3528
 endef
 TARGET_DEVICES += hinlink_h29k
