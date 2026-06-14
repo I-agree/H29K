a8d859831610e69f6170d36699f691822fa2ab47
I-agree <94974975+I-agree@users.noreply.github.com>
2026-06-15 05:01:01 +0800

    Add files via upload

---
+++ scripts/gen_image_generic.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+# Copyright (C) 2006-2012 OpenWrt.org
+set -e
+
+if [ $# -ne 5 ] && [ $# -ne 6 ]; then
+    echo "SYNTAX: $0 <file> <kernel size> <kernel directory> <rootfs size> <rootfs image> [<align>]"
+    exit 1
+fi
+
+OUTPUT="$1"
+KERNELSIZE="$2"
+KERNELDIR="$3"
+KERNELPARTTYPE=${KERNELPARTTYPE:-83}
+ROOTFSSIZE="$4"
+ROOTFSIMAGE="$5"
+ROOTFSPARTTYPE=${ROOTFSPARTTYPE:-83}
+ALIGN="$6"
+
+rm -f "$OUTPUT"
+
+head=16
+sect=63
+
+# create partition table
+if [ -n "$GUID" ]; then
+    # 🌟 核心修复：GPT 模式下移除 -t 规避传统 MBR 83 类型污染；
+    # 🌟 安全兜底：如果 PARTOFFSET 为空，强制指定 @32m 绝对偏移，誓死保护 Rockchip U-Boot 引导区，添加：-D 禁用自动生成BIOS Boot分区，保持2个分区的预期布局
+    BOOTOFFSET="${PARTOFFSET:-32m}"
+    set $(ptgen -o "$OUTPUT" -h $head -s $sect -g -D \
+        -p "${KERNELSIZE}m@${BOOTOFFSET}" \
+        -p "${ROOTFSSIZE}m" \
+        ${SIGNATURE:+-S 0x$SIGNATURE} -G "$GUID")
+else
+    set $(ptgen -o "$OUTPUT" -h $head -s $sect \
+        -t "${KERNELPARTTYPE}" -p "${KERNELSIZE}m" \
+        -t "${ROOTFSPARTTYPE}" -p "${ROOTFSSIZE}m" \
+        ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE})
+fi
+
+KERNELOFFSET="$(($1 / 512))"
+KERNELSIZE="$2"
+ROOTFSOFFSET="$(($3 / 512))"
+ROOTFSSIZE="$(($4 / 512))"
+
+# Using mcopy -s ... is using READDIR(3) to iterate through the directory
+# entries, hence they end up in the FAT filesystem in traversal order which
+# breaks reproducibility.
+# Implement recursive copy with reproducible order.
+dos_dircopy() {
+    local entry
+    local baseentry
+    for entry in "$1"/* ; do
+        if [ -f "$entry" ]; then
+            mcopy -i "$OUTPUT.kernel" "$entry" ::"$2"
+        elif [ -d "$entry" ]; then
+            baseentry="$(basename "$entry")"
+            mmd -i "$OUTPUT.kernel" ::"$2""$baseentry"
+            dos_dircopy "$entry" "$2""$baseentry"/
+        fi
+    done
+}
+
+[ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc count="$ROOTFSSIZE"
+dd if="$ROOTFSIMAGE" of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc
+
+if [ -n "$GUID" ]; then
+    [ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$((ROOTFSOFFSET + ROOTFSSIZE))" conv=notrunc count="$sect"
+    # 强刷标准 FAT32 格式，确保主线 U-Boot 100% 识别内核
+    mkfs.fat --invariant -F 32 -n kernel -C "$OUTPUT.kernel" -S 512 "$((KERNELSIZE / 1024))"
+    LC_ALL=C dos_dircopy "$KERNELDIR" /
+else
+    make_ext4fs -J -L kernel -l "$KERNELSIZE" ${SOURCE_DATE_EPOCH:+-T ${SOURCE_DATE_EPOCH}} "$OUTPUT.kernel" "$KERNELDIR"
+fi
+
+dd if="$OUTPUT.kernel" of="$OUTPUT" bs=512 seek="$KERNELOFFSET" conv=notrunc
+rm -f "$OUTPUT.kernel"
---
+++ target/linux/rockchip/image/armv8.mk
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2020 Sarah Maedel
+
+define Device/rk3528
+  SOC := rk3528
+  KERNEL_LOADADDR := 0x03000000
+endef
+
+define Device/hinlink_h29k
+  $(Device/rk3528)
+  SUBTARGET := armv8
+  DEVICE_VENDOR := HINLINK
+  DEVICE_MODEL := H29K
+  DEVICE_DTS := rk3528-hinlink-h29k
+  UBOOT_CONFIG := hinlink-h29k-rk3528
+  UBOOT_DEVICE_NAME := hinlink-h29k-rk3528
+  KERNEL_LOADADDR := 0x03000000
+  BOOT_SCRIPT := mmc
+  IMAGES := factory.img.gz
+  IMAGE/factory.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := \
+    kmod-usb3 kmod-aic8800-sdio -dnsmasq dnsmasq-full \
+    kmod-usb-net-cdc-mbim uqmi qmi-utils kmod-usb-serial-option kmod-usb-net-rndis-host \
+    kmod-usb-net-cdc-ether kmod-usb-wdm u-boot-hinlink-h29k-rk3528
+endef
+TARGET_DEVICES += hinlink_h29k
