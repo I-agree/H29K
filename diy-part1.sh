5fd3e37d467493384982a70f1eea6dea9a568298
I-agree <94974975+I-agree@users.noreply.github.com>
2026-08-04 13:17:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -80,6 +80,7 @@ download_and_check "${BASE_URL}/package/kernel/axs5106/src/Makefile" "package/ke
 download_and_check "${BASE_URL}/package/kernel/axs5106/src/chipone_axs5106.c" "package/kernel/axs5106/src/chipone_axs5106.c"
 download_and_check "${BASE_URL}/JPG/system" "files/etc/config/system"
 download_and_check "${BASE_URL}/JPG/wireless" "files/etc/config/wireless"
+download_and_check "${BASE_URL}/JPG/smartdns" "files/etc/config/smartdns"
 
 # --- 统一拉取应用层开机 LOGO 组 ---
 for i in 1 2 3; do
