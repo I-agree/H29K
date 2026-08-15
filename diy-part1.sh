92e6c4f53992e384a72dd3ec1644d7f2e891c716
I-agree <94974975+I-agree@users.noreply.github.com>
2026-08-15 14:44:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,7 +33,7 @@ mkdir -p target/linux/rockchip/files/arch/arm64/boot/dts/rockchip \
          files/lib/firmware \
          package/boot/uboot-rockchip/patches \
          package/kernel/aic8800/patches \
-         feeds/packages/lang/python/python-charset-normalizer/patches \
+         package/python-charset-normalizer/patches \
          files/usr/share/fonts
 
 BASE_URL="https://raw.githubusercontent.com/I-agree/H29K/main"
@@ -87,7 +87,7 @@ download_and_check "${BASE_URL}/JPG/wireless" "files/etc/config/wireless"
 download_and_check "${BASE_URL}/JPG/smartdns" "files/etc/config/smartdns"
 download_and_check "${BASE_URL}/JPG/minidlna" "files/etc/config/minidlna"
 download_and_check "${BASE_URL}/JPG/samba4" "files/etc/config/samba4"
-download_and_check "${BASE_URL}/JPG/python-charset-normalizer/patches/999-allow-setuptools-builds.patch" "feeds/packages/lang/python/python-charset-normalizer/patches/999-allow-setuptools-builds.patch"
+download_and_check "${BASE_URL}/package/python-charset-normalizer/Makefile" "package/python-charset-normalizer/Makefile"
 
 # --- 统一拉取应用层开机 LOGO 组 ---
 for i in 1 2 3; do
